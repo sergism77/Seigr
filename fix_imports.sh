9888c26d4e8f4b2fb09be14e4bd76d60b00ae91f
Sergi S.M. <sergism@gmail.com>
2025-01-04 12:23:17 +0100

    Updated .gitignore and removed unnecessary tracked files

--- fix_imports.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Adjust imports in compiled Protobuf files
-find src/seigr_protocol/compiled -name "*.py" -exec sed -i 's/^import \([a-zA-Z_]*_pb2\)/from . import \1/' {} +
-
-echo "✅ Imports in compiled Protobuf files have been fixed."
